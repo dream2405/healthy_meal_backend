c88e16585cbfe831aebb83ce8393beb1b1611bfc
dream2405 <dream2405@naver.com>
2025-04-28 16:18:10 +0900

    Initial commit

---
+++ src/main/java/kr/ac/dankook/ace/healthy_meal_backend/HealthyMealBackendApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.dankook.ace.healthy_meal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyMealBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthyMealBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/dankook/ace/healthy_meal_backend/HealthyMealBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.dankook.ace.healthy_meal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyMealBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
