f4e6bd960e7f12e9ff47271c86e6415f4e3e9941
dream2405 <dream2405@naver.com>
2025-05-19 20:41:27 +0900

    서비스 주석 처리 및 로그 파일 추가

--- src/main/java/kr/ac/dankook/ace/healthy_meal_backend/service/FileSystemStorageService.java
+++ src/main/java/kr/ac/dankook/ace/healthy_meal_backend/service/FileSystemStorageService.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileSystemStorageService {
     private Path rootLocation;
 
-    public void store(MultipartFile file) {
+    /*public void store(MultipartFile file) {
         try {
             if (file.isEmpty()) {
                 throw new StorageException("Failed to store empty file.");
@@ -82,5 +82,5 @@ public class FileSystemStorageService {
         catch (IOException e) {
             throw new StorageException("Could not initialize storage", e);
         }
-    }
+    }*/
 }
