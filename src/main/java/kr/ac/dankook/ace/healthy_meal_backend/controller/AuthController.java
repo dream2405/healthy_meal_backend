b9904d19e1f4e31d7f37d82a2993662951a8b07e
dream2405 <dream2405@naver.com>
2025-09-21 18:03:45 +0900

    예외처리 코드 개선 및 컨트롤러 부분 코드 리펙토링

--- src/main/java/kr/ac/dankook/ace/healthy_meal_backend/controller/AuthController.java
+++ src/main/java/kr/ac/dankook/ace/healthy_meal_backend/controller/AuthController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
